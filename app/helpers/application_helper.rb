31e986c691950d037f6c6b481fc11cd5da450fc8
tikijian <timurt1988@gmail.com>
2014-06-26 11:52:16 +0400

    auth/reg prototype

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def link_to_sign_in
+    if @current_user
+      link_to @current_user.email, destroy_user_session_path, method: :delete, title: 'Выйти'
+    else
+      link_to 'Вход', '#', 'data-toggle' => "modal", 'data-target' => ".login-modal"
+    end
+  end
 end
